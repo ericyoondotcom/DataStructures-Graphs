add43d96da45182970351c98cfdd527f497b3285
Eric <emjyoon@gmail.com>
2017-12-26 19:17:26 -0800

    Added classes

---
+++ Graphs/Graphs/SimpleGraph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+    public class SimpleGraph<T>
+    {
+        public List<SimpleVertex<T>> vertices;
+
+        public SimpleGraph()
+        {
+            
+        }
+    }
+}
---
+++ Graphs/Graphs/SimpleVertex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+    public class SimpleVertex<T>
+    {
+        public List<SimpleVertex<T>> neighbors;
+
+        public SimpleVertex()
+        {
+            
+        }
+    }
+}
