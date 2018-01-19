6d7067fb52273e313cfaa2532a0349d59632f306
Eric <emjyoon@gmail.com>
2018-01-18 17:29:00 -0800

    Started to do a weighted directed

---
+++ Graphs/Graphs/WeightedDirectedGraph.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+	public class WeightedDirectedGraph<T> where T : IComparable
+	{
+        public List<WeightedDirectedVertex<T>> vertices;
+
+        public WeightedDirectedGraph()
+		{
+            vertices = new List<WeightedDirectedVertex<T>>();
+		}
+        public WeightedDirectedGraph(List<WeightedDirectedVertex<T>> vertices)
+		{
+			this.vertices = vertices;
+		}
+
+        public void AddEdge(WeightedDirectedVertex<T> fromNode, WeightedDirectedVertex<T> toNode, float weight)
+		{
+            WeightedDirectedVertex<T> aVertex = vertices.Find(i => i == fromNode);
+            WeightedDirectedVertex<T> bVertex = vertices.Find(i => i == toNode);
+            aVertex.edges.Add(new Tuple<WeightedDirectedVertex<T>, float>(bVertex, weight));
+
+		}
+
+        public WeightedDirectedVertex<T> AddVertex(T newValue) => AddVertex(new WeightedDirectedVertex<T>(newValue));
+
+        public WeightedDirectedVertex<T> AddVertex(WeightedDirectedVertex<T> newVertex)
+		{
+			vertices.Add(newVertex);
+			foreach (WeightedDirectedVertex<T> i in vertices)
+			{
+				if (i == newVertex)
+				{
+					return i;
+				}
+			}
+			throw new Exception("Can't find the newly added Vertex!");
+		}
+
+		public void RemoveVertex(WeightedDirectedVertex<T> vertex)
+		{
+			foreach (WeightedDirectedVertex<T> possibleNeighbor in vertices)
+			{ //any better way to do this? Is there a List.Map? Can I use List.ConvertAll?
+                if (possibleNeighbor.edges.Find)
+				{
+					possibleNeighbor.neighbors.Remove(vertex);
+				}
+			}
+			vertices.Remove(vertex);
+		}
+		public void RemoveVertex(int index)
+		{
+			RemoveVertex(vertices[index]);
+		}
+
+		public bool HasVertex(SimpleVertex<T> vertex) => vertices.Contains(vertex);
+
+		List<SimpleVertex<T>> visited;
+		SimpleVertex<T> item;
+		public SimpleVertex<T> DepthFirstTraversal(SimpleVertex<T> root, T searchVal)
+		{
+			item = null;
+			visited = new List<SimpleVertex<T>>();
+			DepthFirstTraversalHelper(root, searchVal);
+			return item;
+		}
+
+		void DepthFirstTraversalHelper(SimpleVertex<T> thisNode, T searchVal)
+		{
+			if (item != null) return;
+			foreach (SimpleVertex<T> n in thisNode.neighbors)
+			{
+				if (visited.Contains(n)) continue;
+				if (n.val.CompareTo(searchVal) == 0)
+				{
+					item = n;
+					return;
+				}
+
+				visited.Add(n);
+				DepthFirstTraversalHelper(n, searchVal);
+
+			}
+
+		}
+
+		Queue<SimpleVertex<T>> q;
+
+		public SimpleVertex<T> BreadthFirstTraversal(SimpleVertex<T> root, T searchVal)
+		{
+			visited = new List<SimpleVertex<T>>();
+			q = new Queue<SimpleVertex<T>>();
+
+			q.Enqueue(root);
+			while (q.Count != 0)
+			{
+
+				var n = q.Peek();
+				if (n.val.CompareTo(searchVal) == 0)
+				{
+					return n;
+				}
+				q.Dequeue();
+				foreach (var i in n.neighbors)
+				{
+					if (!visited.Contains(i)) q.Enqueue(i);
+				}
+				visited.Add(n);
+
+			}
+
+			Console.WriteLine("hi!");
+			return null;
+		}
+	}
+}
---
+++ Graphs/Graphs/WeightedDirectedVertex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+
+namespace Graphs
+{
+	public class WeightedDirectedVertex<T>
+	{
+        public List<Tuple<WeightedDirectedVertex<T>, float>> edges;
+		public T val;
+        public WeightedDirectedVertex(T val, List<Tuple<WeightedDirectedVertex<T>, float>> neighbors = null)
+		{
+			if (neighbors == null)
+			{
+                List<(string Name, int Age)> list = new List<(string, int)>();
+                var data = list.Where(t => t.Age > 3);
+                                                                  
+                //(string test, int bla) value1 = ("", 3);
+                //data.RemoveAt(data.FindIndex(x => x.firstVal == "what youre looking for"))
+
+                this.edges = new List<Tuple<WeightedDirectedVertex<T>, float>>();
+			}
+			else
+			{
+                this.edges = neighbors;
+			}
+			this.val = val;
+		}
+	}
+}
